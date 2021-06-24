42d28a85150c156861e0dc22c4b35959aab66b03
tgr-pear <85336647+tgr-pear@users.noreply.github.com>
2021-06-24 16:50:41 +0900

    ex02 ex03

---
+++ src/ex100/ex02.java
@@ -0,0 +1,13 @@
+package ex100;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		// 콘솔에 println 또는 print문을 이용하여 다음 문장을 출력하시오.
+		 /* 입력 예시 : 없음 / 
+	  		출력 예시 : Hello World */
+		
+		System.out.println( "Hello " + "World" );
+	}
+
+}
---
+++ src/ex100/ex03.java
@@ -0,0 +1,19 @@
+package ex100;
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		// 콘솔에 println 또는 print문을 이용하여 다음 문장을 출력하시오.
+		 /* 입력 예시 : 없음 / 
+			출력 예시 : Hello 
+		  				World */
+		/* 도움말 : \n은 줄바꿈 특수문자 
+		 			문자열은 " / 문자 한 자는 ' 사용 */
+		
+		System.out.println("Hello" + '\n' + "World");
+		/* System.out.println( "Hello" );
+	  	   System.out.println( "World" ); */
+		
+	}
+
+}
